c1b2813732325db74c183d7ca9eb6644227e5e18
vikashage21 <vk220783@gmail.com>
2023-10-04 14:27:49 +0530

    learning operations in js

---
+++ basics/operations.js
@@ -0,0 +1,15 @@
+
+// tricky questions 
+
+console.log(1+1+"2");
+console.log("1"+1+1);
+console.log(1+"1"+1);
+
+let value=1;
+let natValue=-value;
+
+console.log(natValue);
+// this type of code is not accpeted by company // becouse it  more complex to understand.
+let num1 ,num2 ,num3;
+num1=num2=num3=2+2;
+console.log(num1)
