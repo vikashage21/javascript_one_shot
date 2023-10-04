1bbf96ce2674a4d0462f8c4dee0b246ab401a9c1
vikashage21 <vk220783@gmail.com>
2023-10-04 14:01:20 +0530

    conversion_opertions in js

---
+++ basics/conversion_operations.js
@@ -0,0 +1,32 @@
+//learning operations
+
+// let score=33;
+
+let score="33aaa";
+
+let valueNumber=Number(score);
+// console.log(typeof valueNumber)
+// console.log( valueNumber)
+
+// nan not a number
+//"33"=>33
+// "33abc"=> nan
+// true=> 1; false=>0
+
+let booleanTure= 1;
+ let booleanCovert= Boolean(booleanTure)
+// console.log(typeof booleanTure);
+// console.log( typeof booleanCovert);
+// console.log( booleanCovert);
+
+// notes
+
+//  1 => true
+//  ""=> false
+//  "vikash" true
+
+
+let someString=33;
+let stringCovert= String(someString)
+console.log(stringCovert);
+console.log(typeof stringCovert);
