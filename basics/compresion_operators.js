16ef95c4430ef45d7b9615622dae2f9da09418b7
vikashage21 <vk220783@gmail.com>
2023-10-04 15:08:35 +0530

    learning compresion operators in js

---
+++ basics/compresion_operators.js
@@ -0,0 +1,15 @@
+console.log("2">1);;
+console.log("02">1);
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+//notes 
+
+// the reason is that an equality check == and comparisons > < >= <= work differently.
+// comparisons convert null to  a number , terating is as0.
+// that,s why (3) null >= 0 is true and (1) null > 0 is false 
+
+
+console.log("2"===2);
+
