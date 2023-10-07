5f8c0aa89ce286e71dbe87242738aa0376c1f7a4
vikashage21 <vk220783@gmail.com>
2023-10-07 14:25:24 +0530

    learning up to dates in js

---
+++ basics/dates.js
@@ -0,0 +1,24 @@
+// learning dates in js
+
+const myDate= new Date()
+
+console.log(myDate)
+console.log(myDate.toString())
+console.log(typeof myDate)
+
+
+// console.log(myDate.toDateString())
+// console.log(myDate.toISOString())
+// console.log(myDate.toJSON())
+// console.log(myDate.getDate())
+// console.log(myDate.toLocaleString({
+  
+// }))
+
+
+const timeSpan=new Date()
+console.log(Date.now())
+
+
+
+
